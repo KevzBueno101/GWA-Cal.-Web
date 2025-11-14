8d0907e5679e730a01b42d2fc0da9333624c6bc0
Kevin Bueno <196433083+KevzBueno101@users.noreply.github.com>
2025-11-15 00:08:31 +0800

    Delete web directory

--- web/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.querySelector('.hamburger');
-    const navbar = document.querySelector('.navbar');
-
-    hamburger.addEventListener('click', function() {
-        navbar.classList.toggle('open');
-        hamburger.classList.toggle('open');
-    });
-});
